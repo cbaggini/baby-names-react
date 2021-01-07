875ca87f0ad966550e21a13b3a76586463b2ce64
Cecilia <earwen1986@gmail.com>
2021-01-07 14:59:16 +0000

    Completed level 2

--- src/NamesList.js
+++ src/NamesList.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import BabyName from "./BabyName";
 import babyNamesData from "./data/babyNamesData.json";
 
 const NamesList = () => {
 	const babyNamesSorted = babyNamesData.sort((a,b) => a.name > b.name);
+	const [filteredNames, setFilteredNames] = useState(babyNamesSorted);
+
+	const filterNames = (e) => {
+		if (e.target.value) {
+			const newNames = babyNamesSorted.filter(el => el.name.toLowerCase().includes(e.target.value.toLowerCase()))
+			setFilteredNames(newNames);
+		} else {
+			setFilteredNames(babyNamesSorted);
+		}	
+	}
+
 	return (
-	<div className="names">
-		{babyNamesSorted.map((el) => <BabyName key={el.id} {... el}/>)}
+	<>
+	<div className="searchBar">
+		<input type="text" onChange={filterNames}></input>
 	</div>
+	<div className="names">
+		{filteredNames.map((el) => <BabyName key={el.id} {... el}/>)}
+	</div>
+	</>
 	);
 }
 
