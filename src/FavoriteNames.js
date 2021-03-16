3dcec58012007bf02f799297035d4345b3172c95
Cecilia <earwen1986@gmail.com>
2021-03-16 17:39:47 +0000

    Minor formatting

--- src/FavoriteNames.js
+++ src/FavoriteNames.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react";
 import BabyName from "./BabyName";
 
-const FavoriteNames = ({favorite, changeFavorite}) => {
-	//console.log(favorite)
-	return (
-		<div className="favoriteNames">
-			<h2>Favourite names: </h2>
-			{favorite.map(el => <BabyName key={el.id} {... el} changeFavorite={changeFavorite}/>)}
-		</div>
-	);
-}
+const FavoriteNames = ({ favorite, changeFavorite }) => {
+  return (
+    <div className="favoriteNames">
+      <h2>Favourite names: </h2>
+      {favorite.map((el) => (
+        <BabyName key={el.id} {...el} changeFavorite={changeFavorite} />
+      ))}
+    </div>
+  );
+};
 
 export default FavoriteNames;
