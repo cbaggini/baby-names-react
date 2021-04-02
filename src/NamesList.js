807ed49e75182b30beaa5b0680c94c6b2936bebe
Cecilia <earwen1986@gmail.com>
2021-04-02 12:45:32 +0100

    Changed formatting

--- src/NamesList.js
+++ src/NamesList.js
@@ -2,17 +2,22 @@ import React from "react";
 import BabyName from "./BabyName";
 import SearchBar from "./SearchBar";
 
-
-const NamesList = ({filteredNames, filterNames, changeFavorite, toggleGenders}) => {
-
-	return (
-	<>
-	<SearchBar filterNames={filterNames} toggleGenders={toggleGenders}/>
-	<div className="names">
-		{filteredNames.names.map((el) => <BabyName key={el.id} {... el} changeFavorite={changeFavorite}/>)}
-	</div>
-	</>
-	);
-}
+const NamesList = ({
+  filteredNames,
+  filterNames,
+  changeFavorite,
+  toggleGenders,
+}) => {
+  return (
+    <>
+      <SearchBar filterNames={filterNames} toggleGenders={toggleGenders} />
+      <div className="names">
+        {filteredNames.names.map((el) => (
+          <BabyName key={el.id} {...el} changeFavorite={changeFavorite} />
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default NamesList;
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,15 +1,24 @@
 import React from "react";
 
-const SearchBar = ({filterNames, toggleGenders}) => {
-
-	return (
-	<div className="searchBar">
-		<input type="text" onChange={filterNames} placeholder="Search for names..."></input>
-		<button id="all" onClick={toggleGenders} className="mf selected">All</button>
-		<button id="f" onClick={toggleGenders} className="f">Girls</button>
-		<button id="m" onClick={toggleGenders} className="m">Boys</button>
-	</div>
-	);
+const SearchBar = ({ filterNames, toggleGenders }) => {
+  return (
+    <div className="searchBar">
+      <input
+        type="text"
+        onChange={filterNames}
+        placeholder="Search for names..."
+      ></input>
+      <button id="all" onClick={toggleGenders} className="mf selected">
+        All
+      </button>
+      <button id="f" onClick={toggleGenders} className="f">
+        Girls
+      </button>
+      <button id="m" onClick={toggleGenders} className="m">
+        Boys
+      </button>
+    </div>
+  );
 };
 
 export default SearchBar;
